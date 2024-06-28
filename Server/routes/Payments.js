dfd262d7c4c62611a9a194d88a4f3913a7f25ca9
aasn0119 <abhitrivedi119@gmail.com>
2024-06-28 17:19:01 +0530

    chore: Refactor payment process and add RazorPay integration

--- Server/routes/Payments.js
+++ Server/routes/Payments.js
@@ -8,6 +8,7 @@ const {
   sendPaymentSuccessEmail,
 } = require("../controllers/Payments");
 const { auth, isStudent } = require("../middlewares/auth");
+
 router.post("/capturePayment", auth, isStudent, capturePayment);
 router.post("/verifyPayment", auth, isStudent, verifyPayment);
 router.post("/sendPaymentSuccessEmail", auth, isStudent, sendPaymentSuccessEmail);
